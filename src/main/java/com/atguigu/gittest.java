c50166c8da76e0d62471aacdbbfb2f71c37ffa94
kaiyu <Azkaban0225@163.com>
2021-06-26 22:17:54 +0800

    fist commit

---
+++ src/main/java/com/atguigu/gittest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author wky
+ * @create 2021-06-26-22:02
+ */
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("gidea github");
+    }
+}
