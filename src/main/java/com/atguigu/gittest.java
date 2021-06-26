7333888305301f98fe081813d87933218eb5b0c9
kaiyu <Azkaban0225@163.com>
2021-06-26 22:25:44 +0800

    hot-fix

--- src/main/java/com/atguigu/gittest.java
+++ src/main/java/com/atguigu/gittest.java
@@ -7,5 +7,7 @@ package com.atguigu;
 public class gittest {
     public static void main(String[] args) {
         System.out.println("gidea github");
+        System.out.println("hot-fix");
+        System.out.println("11");
     }
 }
