d2fee616190c88034f4ab59123561c2e4ec0b01a
kaiyu <Azkaban0225@163.com>
2021-06-26 22:27:44 +0800

    master

--- src/main/java/com/atguigu/gittest.java
+++ src/main/java/com/atguigu/gittest.java
@@ -1,11 +1,9 @@
 package com.atguigu;
 
-/**
- * @author wky
- * @create 2021-06-26-22:02
- */
+
 public class gittest {
     public static void main(String[] args) {
         System.out.println("gidea github");
+        System.out.println("master");
     }
 }
